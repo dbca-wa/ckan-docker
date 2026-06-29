ba312f2e602649913fd6acbf7eef1619ce44f2c9
Mark Calvert <markcalvert84@gmail.com>
2026-06-29 15:50:54 +1200

    CKAN 2.11 Debian migration: build, startup, supervisor and CI scripts
    - dbca_requirements.sh: apk->apt (geos/proj/file/unzip); qsv musl->gnu binary;
      install supervisor (not shipped by the Debian base); add undeclared deps
      shapely (spatial) and geojson (dbca).
    - Dockerfile.worker: root setup then drop to ckan; ckan-sys group; /usr/local
      paths; worker confs into /etc/supervisord.d/ (trailing slash).
    - supervisor confs + cron: /usr/bin/ckan -> /usr/local/bin/ckan; supervisord
      group -> ckan-sys.
    - start_ckan / start_ckan_development overrides: generate SECRET_KEY (the 2.11
      config key) instead of beaker.session.secret.
    - .env.example: CKAN_VERSION 2.11.0, SOLR_IMAGE_VERSION 2.11-solr9.
    - dbca_build.yml: checkout@v6->v7; fix worker CKAN_IMAGE build-arg to use a
      single resolved tag.

--- ckan/setup/dbca_requirements.sh
+++ ckan/setup/dbca_requirements.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+## Process management ##
+# supervisor runs the CKAN job workers (not shipped by the Debian base). Installed
+# here so the worker image and the shared dev site-packages volume both have it.
+pip3 install supervisor
+
 ## CKAN Core extensions ##
 
 # Archiver
@@ -37,15 +42,16 @@ pip3 install -r ${SRC_DIR}/ckanext-showcase/requirements.txt
 # Spatial
 # dependencies
 export PROJ_DIR=/usr
-apk add --no-cache \
-    geos \
-    geos-dev \
-    proj-util \
-    proj-dev 
+apt-get update && apt-get install -y --no-install-recommends \
+    libgeos-dev \
+    libproj-dev \
+    proj-bin
 pip3 install -e git+https://github.com/ckan/ckanext-spatial.git@master#egg=ckanext-spatial
-# pyproj 3.6.1 in ckanext-spatial requirements.txt is not working
-# Download the CKAN spatial extension requirements file and remove the line pyproj==3.6.1; python_version >= '3.9'
-curl -sSL https://raw.githubusercontent.com/ckan/ckanext-spatial/v2.3.0/requirements.txt \
+# pyproj 3.6.1 in ckanext-spatial requirements.txt is not working: download the
+# v2.3.0 requirements (which also pin Shapely/geojson/OWSLib) and strip that
+# pyproj line so we can install a later compatible pyproj below. Use wget (the
+# Debian base does not ship curl).
+wget -qO- https://raw.githubusercontent.com/ckan/ckanext-spatial/v2.3.0/requirements.txt \
     | sed 's/pyproj==3.6.1; python_version >= '\''3.9'\''//g' > ${SRC_DIR}/ckanext-spatial/requirements.txt
 # manually install a later compatible version of pyproj
 pip3 install pyproj==3.7.*
@@ -64,6 +70,8 @@ pip3 install -r ${SRC_DIR}/ckanext-xloader/requirements.txt
 pip3 install -e git+https://github.com/jqnatividad/ckanext-officedocs.git@master#egg=ckanext-officedocs
 
 # SAML2
+# pysaml2 shells out to the xmlsec1 binary for XML signing
+apt-get update && apt-get install -y --no-install-recommends xmlsec1
 pip3 install -e git+https://github.com/keitaroinc/ckanext-saml2auth.git@v1.3.0#egg=ckanext-saml2auth
 
 ## DBCA Project ##
@@ -76,9 +84,11 @@ pip3 install -e git+https://github.com/dbca-wa/ckanext-doi@develop#egg=ckanext-d
 
 # QA
 # Install qsv dependency for extension ckanext-qa
-wget -O /tmp/qsv.zip https://github.com/jqnatividad/qsv/releases/download/0.110.0/qsv-0.110.0-x86_64-unknown-linux-musl.zip
+# file (libmagic) is required by ckanext-qa; unzip is needed to extract the qsv binary
+apt-get update && apt-get install -y --no-install-recommends file unzip
+# Use the glibc (gnu) qsv build for Debian; the musl build only runs on Alpine
+wget -O /tmp/qsv.zip https://github.com/jqnatividad/qsv/releases/download/0.110.0/qsv-0.110.0-x86_64-unknown-linux-gnu.zip
 unzip /tmp/qsv.zip -d /usr/local/bin
 rm /tmp/qsv.zip
-apk add file
 pip3 install -e git+https://github.com/dbca-wa/ckanext-qa.git@develop#egg=ckanext-qa
 pip3 install -r ${SRC_DIR}/ckanext-qa/requirements.txt
